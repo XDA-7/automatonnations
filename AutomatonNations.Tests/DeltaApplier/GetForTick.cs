8ed0075dd21270d9f3975ca5ebc661d579618b98
XDA-7 <xaraxsaren@hotmail.com.au>
2018-04-18 20:25:44 +1000

    .

--- AutomatonNations.Tests/DeltaApplier/GetForTick.cs
+++ AutomatonNations.Tests/DeltaApplier/GetForTick.cs
@@ -14,11 +14,15 @@ namespace AutomatonNations
         private SimulationView _simulationView = new SimulationView
         {
             Simulation = new Simulation(),
-            StarSystems = new StarSystem[0]
+            StarSystems = new StarSystem[0],
+            Empires = new Empire[0],
+            Wars = new War[0]
         };
         private DeltaSet _deltaSet = new DeltaSet
         {
-            DeltaDoubles = new Delta<double>[0]
+            Deltas = new Delta[0],
+            DeltaDoubles = new Delta<double>[0],
+            DeltaObjectIds = new Delta<ObjectId>[0]
         };
 
         public GetForTick()
@@ -75,7 +79,7 @@ namespace AutomatonNations
 
             var result = _deltaApplier.GetForTick(It.IsAny<ObjectId>(), It.IsAny<int>());
             var resultEmpire = Assert.Single(result.Empires);
-            Assert.Equal(0, resultEmpire.StarSystemsIds.Count());
+            Assert.Empty(resultEmpire.StarSystemsIds);
         }
 
         [Fact]
@@ -92,8 +96,8 @@ namespace AutomatonNations
 
             var result = _deltaApplier.GetForTick(It.IsAny<ObjectId>(), It.IsAny<int>());
             var resultEmpire = Assert.Single(result.Empires);
-            Assert.Equal(1, resultEmpire.StarSystemsIds.Count());
-            Assert.Equal(starSystem.Id, resultEmpire.StarSystemsIds.Single());
+            var resultSystemId = Assert.Single(resultEmpire.StarSystemsIds);
+            Assert.Equal(starSystem.Id, resultSystemId);
         }
 
         [Fact]
@@ -127,14 +131,47 @@ namespace AutomatonNations
 
         [Fact]
         public void AppliesWarAttackerDamage()
-        {}
+        {
+            var war = new War { Id = ObjectId.GenerateNewId(), AttackerDamage = 500.0 };
+            _simulationView.Wars = new War[] { war };
+            _deltaSet.DeltaDoubles = new Delta<double>[]
+            {
+                new Delta<double> { DeltaType = DeltaType.WarAttackerDamage, ReferenceId = war.Id, Value = 216.5 }
+            };
+
+            var result = _deltaApplier.GetForTick(It.IsAny<ObjectId>(), It.IsAny<int>());
+            var resultWar = Assert.Single(result.Wars);
+            Assert.Equal(283.5, war.AttackerDamage);
+        }
 
         [Fact]
         public void AppliesWarDefenderDamage()
-        {}
+        {
+            var war = new War { Id = ObjectId.GenerateNewId(), DefenderDamage = 500.0 };
+            _simulationView.Wars = new War[] { war };
+            _deltaSet.DeltaDoubles = new Delta<double>[]
+            {
+                new Delta<double> { DeltaType = DeltaType.WarDefenderDamage, ReferenceId = war.Id, Value = 216.5 }
+            };
+
+            var result = _deltaApplier.GetForTick(It.IsAny<ObjectId>(), It.IsAny<int>());
+            var resultWar = Assert.Single(result.Wars);
+            Assert.Equal(283.5, war.DefenderDamage);
+        }
 
         [Fact]
         public void AppliesEmpireMilitary()
-        {}
+        {
+            var empire = new Empire { Id = ObjectId.GenerateNewId(), StarSystemsIds = new ObjectId[0], Military = 230.0 };
+            _simulationView.Empires = new Empire[] { empire };
+            _deltaSet.DeltaDoubles = new Delta<double>[]
+            {
+                new Delta<double> { DeltaType = DeltaType.EmpireMilitary, ReferenceId = empire.Id, Value = 45.0 }
+            };
+
+            var result = _deltaApplier.GetForTick(It.IsAny<ObjectId>(), It.IsAny<int>());
+            var resultEmpire = Assert.Single(result.Empires);
+            Assert.Equal(185.0, empire.Military);
+        }
     }
 }
--- AutomatonNations/Process/DeltaApplier.cs
+++ AutomatonNations/Process/DeltaApplier.cs
@@ -20,6 +20,10 @@ namespace AutomatonNations
             var simulationView = _simulationRepository.GetSimulationView(simulationId);
             var deltaSet = _deltaRepository.GetForSimulation(simulationId, tick, simulationView.Simulation.Ticks);
             simulationView.StarSystems = ApplyDevelopmentDeltas(simulationView.StarSystems, deltaSet.DeltaDoubles);
+            simulationView.Empires = ApplyMilitaryDeltas(simulationView.Empires, deltaSet.DeltaDoubles);
+            simulationView.Empires = ApplySystemTransferDeltas(simulationView.Empires, deltaSet.DeltaObjectIds);
+            simulationView.Wars = ApplyWarDeltas(simulationView.Wars, deltaSet.Deltas);
+            simulationView.Wars = ApplyWarDamageDeltas(simulationView.Wars, deltaSet.DeltaDoubles);
             return simulationView;
         }
 
@@ -32,6 +36,76 @@ namespace AutomatonNations
                     .Where(delta => delta.ReferenceId == starSystem.Id)
                     .Sum(delta => delta.Value);
                 return starSystem;
+            });
+        }
+
+        private IEnumerable<Empire> ApplySystemTransferDeltas(IEnumerable<Empire> empires, IEnumerable<Delta<ObjectId>> deltas)
+        {
+            var gainDeltas = deltas.Where(x => x.DeltaType == DeltaType.EmpireSystemGain);
+            var lossDeltas = deltas.Where(x => x.DeltaType == DeltaType.EmpireSystemLoss);
+            return empires.Select(empire => {
+                var empireGains = gainDeltas
+                    .Where(x => x.ReferenceId == empire.Id)
+                    .Select(x => x.Value);
+                var empireLosses = lossDeltas
+                    .Where(x => x.ReferenceId == empire.Id)
+                    .Select(x => x.Value);
+                empire.StarSystemsIds = empire.StarSystemsIds
+                    .Where(system => !empireGains.Contains(system))
+                    .Concat(empireLosses);
+                return empire;
+            });
+        }
+
+        private IEnumerable<Empire> ApplyMilitaryDeltas(IEnumerable<Empire> empires, IEnumerable<Delta<double>> deltas)
+        {
+            var militaryDeltas = deltas.Where(x => x.DeltaType == DeltaType.EmpireMilitary);
+            return empires.Select(empire =>
+            {
+                empire.Military -= militaryDeltas
+                    .Where(x => x.ReferenceId == empire.Id)
+                    .Select(x => x.Value)
+                    .Sum();
+                return empire;
+            });
+        }
+
+        private IEnumerable<War> ApplyWarDeltas(IEnumerable<War> wars, IEnumerable<Delta> deltas)
+        {
+            var beginDeltaIds = deltas
+                .Where(x => x.DeltaType == DeltaType.WarBegin)
+                .Select(x => x.ReferenceId);
+            var endDeltaIds = deltas
+                .Where(x => x.DeltaType == DeltaType.WarEnd)
+                .Select(x => x.ReferenceId);
+            return wars
+                .Where(x => !beginDeltaIds.Contains(x.Id))
+                .Select(x =>
+                {
+                    if (endDeltaIds.Contains(x.Id))
+                    {
+                        x.Active = true;
+                    }
+
+                    return x;
+                });
+        }
+
+        private IEnumerable<War> ApplyWarDamageDeltas(IEnumerable<War> wars, IEnumerable<Delta<double>> deltas)
+        {
+            var attackerDamageDeltas = deltas.Where(x => x.DeltaType == DeltaType.WarAttackerDamage);
+            var defenderDamageDeltas = deltas.Where(x => x.DeltaType == DeltaType.WarDefenderDamage);
+            return wars.Select(war =>
+            {
+                war.AttackerDamage -= attackerDamageDeltas
+                    .Where(x => x.ReferenceId == war.Id)
+                    .Select(x => x.Value)
+                    .Sum();
+                war.DefenderDamage -= defenderDamageDeltas
+                    .Where(x => x.ReferenceId == war.Id)
+                    .Select(x => x.Value)
+                    .Sum();
+                return war;
             });
         }
     }
